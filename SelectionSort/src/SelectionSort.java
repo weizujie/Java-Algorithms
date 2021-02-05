f1949ad4a0c078fdff21ab5d6f127af2ac98da64
weizujie <byojiaoxianz7@outlook.com>
2021-02-05 15:38:16 +0800

    快速排序

--- SelectionSort/src/SelectionSort.java
+++ SelectionSort/src/SelectionSort.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class SelectionSort {
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 4, 3, 5};
+        System.out.println(Arrays.toString(selectionSort(arr)));
+    }
+
+
+    public static int[] selectionSort(int[] arr) {
+        int smallNum;
+        int[] newArr = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            smallNum = findSmallest(arr);
+            newArr[i] = smallNum;
+        }
+        return newArr;
+    }
+
+
+    public static int findSmallest(int[] arr) {
+        int smallNum = arr[0];
+        int smallIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < smallNum) {
+                smallNum = arr[i];
+                smallIndex = i;
+            }
+        }
+        return smallIndex;
+    }
+
 }
